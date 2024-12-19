2375cf7bab3eb9e3433074862d17625a31f6361b
NodeRunnerUA <118058348+Vladymirovich@users.noreply.github.com>
2024-12-19 15:51:34 +0400

    Update TelegramBotsConfig.java

--- src/main/java/com/rocketscreener/config/TelegramBotsConfig.java
+++ src/main/java/com/rocketscreener/config/TelegramBotsConfig.java
@@ -1,12 +1,11 @@
 package com.rocketscreener.config;
 
-import com.rocketscreener.controllers.AdminBotController;
-import com.rocketscreener.controllers.PublicBotController;
-import io.github.cdimascio.dotenv.Dotenv;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import com.rocketscreener.controllers.AdminBotController;
+import com.rocketscreener.controllers.PublicBotController;
 
 @Configuration
 public class TelegramBotsConfig {
