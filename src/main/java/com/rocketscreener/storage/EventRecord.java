8d1781e05df8241ac478fb0b64bd1a00105723d6
NodeRunnerUA <118058348+Vladymirovich@users.noreply.github.com>
2024-12-19 16:06:06 +0400

    Update EventRecord.java

--- src/main/java/com/rocketscreener/storage/EventRecord.java
+++ src/main/java/com/rocketscreener/storage/EventRecord.java
@@ -1,14 +1,16 @@
 package com.rocketscreener.storage;
 
 import org.json.JSONObject;
-
+import java.time.LocalDateTime;
 import java.sql.Timestamp;
 
 public record EventRecord(
         int id,
+        String data,
         String eventType,
         String symbol,
         String source,
         Timestamp timestamp,
+        LocalDateTime createdAt,
         JSONObject details
 ){}
